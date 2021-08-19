9aa4137f74938450a08eb2d9d5c6de241b91d280
willnode <willnode@wellosoft.net>
2021-08-19 09:13:27 +0000

    Deploying to gh-pages from @ dasawarsalink/dasawarsa@3cdba0728b07678ce349404f9e2b7bb112b305f4 🚀

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-db1f37e2457f965b1244.js"
+    "url": "webpack-runtime-ca37f6f1e5c0488283dd.js"
   },
   {
     "url": "framework-c54b01a45b8ef1cd9fda.js"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "9857ae92e58ce27269248e3d6bea2c43"
+    "revision": "5abfccb47f77f25d449f3a032e043f5e"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-159248dac574f90588a6.js"
